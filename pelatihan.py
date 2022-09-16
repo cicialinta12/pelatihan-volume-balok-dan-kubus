0127df9d54ee4f1c2fb08251521e9b61b6c44321
cicialinta12 <113221773+cicialinta12@users.noreply.github.com>
2022-09-16 21:32:25 +0700

    pelatihan coding

---
+++ pelatihan.py
@@ -0,0 +1,13 @@
+#volume kubus
+print("menghitung volume kubus")
+s = int(input("masukkan panjang rusuk kubus ="))
+volumekubus = s*s*s
+print("volume kubusnya adalah", volumekubus)
+
+#volume balok
+print("menghitung volume balok")
+p = int(input("masukkan panjang balok="))
+l = int(input("masukkan lebar balok="))
+t = int(input("masukkan tinggi balok="))
+volumebalok = (p*l*t)
+print("volume baloknya adalah", volumebalok)
